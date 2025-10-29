156ec6a2012a8e8c5cdb54fb1d83ce3a3e417af2
amiruldanielcs <106524830+amirulMood@users.noreply.github.com>
2025-10-30 00:02:48 +0800

    first commit

---
+++ src/main/java/com/task/download_client2/DownloadClient2Application.java
@@ -0,0 +1,13 @@
+package com.task.download_client2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadClient2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadClient2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/download_client2/DownloadClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.download_client2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadClient2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
